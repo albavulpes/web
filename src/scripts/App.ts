533afc2f59d81fcb5a166d012db8ed9dcc20d988
cypherix93 <bikram.ghosh93@gmail.com>
2017-06-04 02:28:17 -0400

    Did a bunch of stuff with Vue.

---
+++ src/scripts/App.ts
@@ -0,0 +1,8 @@
+import * as Vue from "vue";
+import * as VueRouter from "vue-router";
+import KeenUI from "keen-ui";
+
+Vue.use(VueRouter);
+Vue.use(KeenUI);
+
+import "./app/components/AppComponent";
---
+++ src/scripts/app/components/AppComponent.ts
@@ -0,0 +1,9 @@
+import Component from "vue-class-component";
+
+@Component({
+    template: require("./AppComponent.html") as string
+})
+export default class AppComponent
+{
+
+}
