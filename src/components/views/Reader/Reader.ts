31b9e8a87a8ea983800416a9ab0c710d85b1d38e
cypherix93 <bikram.ghosh93@gmail.com>
2017-08-24 10:10:53 -0400

    Started fuckin with the reader.

--- src/components/views/Reader/Reader.ts
+++ src/components/views/Reader/Reader.ts
@@ -1,6 +1,43 @@
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Prop, Provide} from 'vue-property-decorator';
 
 @Component
 export default class extends Vue {
+    constructor() {
+        super();
+        this.currentPage = this.pages[0];
+    }
+
+    @Provide()
+    currentPage: string;
+
+    pages = [
+        '08.png',
+        '09.png',
+        '10.png',
+        '11.png',
+        '12.png',
+        '13.png',
+        '14.png'
+    ];
+
+    nextPage() {
+        let index = this.pages.indexOf(this.currentPage);
+        index++;
+
+        if (index >= this.pages.length)
+            index = this.pages.length - 1;
+
+        this.currentPage = this.pages[index];
+    }
+
+    prevPage() {
+        let index = this.pages.indexOf(this.currentPage);
+        index--;
+
+        if (index < 0)
+            index = 0;
+
+        this.currentPage = this.pages[index];
+    }
 }
--- src/components/views/Reader/Reader.vue
+++ src/components/views/Reader/Reader.vue
@@ -2,18 +2,18 @@
     <v-layout row>
         <v-flex xs12>
             <v-layout row align-end>
-                <v-btn fab icon>
+                <v-btn fab icon @click="prevPage">
                     <v-icon>mdi-arrow-left</v-icon>
                 </v-btn>
                 <v-flex xs10>
-                    <h3>Chapter 4 - Page 12</h3>
+                    <h3>{{currentPage}}</h3>
                 </v-flex>
                 <v-flex fill-height class="reader-view-controls text-xs-right">
                     <v-btn fab icon class="control-button">
                         <v-icon>mdi-fullscreen</v-icon>
                     </v-btn>
                 </v-flex>
-                <v-btn fab icon>
+                <v-btn fab icon @click="nextPage">
                     <v-icon>mdi-arrow-right</v-icon>
                 </v-btn>
             </v-layout>
