2ac0011b3b412320e8f58f79b5be4bd4a92192b9
cypherix93 <bikram.ghosh93@gmail.com>
2017-08-23 20:45:03 -0400

    Fixed fonts and added logo with shadow.

--- src/components/layout/TopNav/TopNav.vue
+++ src/components/layout/TopNav/TopNav.vue
@@ -3,20 +3,26 @@
         <v-toolbar fixed scroll-off-screen class="primary">
             <v-toolbar-side-icon class="btn--large hidden-md-and-up"></v-toolbar-side-icon>
             <a href="#" class="headline white--text brand-link">
-                <img class="brand-logo" src="../../../assets/images/logo.png" alt="AlbaVulpes Logo" width="64" height="64">
+                <img class="brand-logo" src="../../../assets/images/logo_shadow.png" alt="AlbaVulpes Logo" width="64" height="64">
                 <v-toolbar-title class="headline">
                     Alba Vulpes
                 </v-toolbar-title>
             </a>
             <v-toolbar-items class="hidden-sm-and-down">
                 <v-btn flat large>
-                    <span class="subheading">Home</span>
+                    <span class="subheading">
+                        Home
+                    </span>
                 </v-btn>
                 <v-btn flat large>
-                    <span class="subheading">Comics</span>
+                    <span class="subheading">
+                        Comics
+                    </span>
                 </v-btn>
                 <v-btn flat large>
-                    <span class="subheading">Art</span>
+                    <span class="subheading">
+                        Art
+                    </span>
                 </v-btn>
             </v-toolbar-items>
         </v-toolbar>
