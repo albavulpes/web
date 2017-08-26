fdc8cf97a9ee5783662427938b0a1d375c122465
cypherix93 <bikram.ghosh93@gmail.com>
2017-08-26 15:50:10 -0400

    Better responsiveness.

--- src/components/views/Reader/Reader.vue
+++ src/components/views/Reader/Reader.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <v-layout row>
-            <v-flex md3 lg2 class="hidden-sm-and-down">
+        <v-layout>
+            <v-flex md3 lg2 fill-height class="hidden-sm-and-down">
                 <v-list dense>
                     <template v-for="page in pages">
                         <v-list-tile :key="page" v-model="currentPages.main" @click="selectPage(page)">
@@ -12,17 +12,17 @@
                     </template>
                 </v-list>
             </v-flex>
-            <v-flex>
-                <v-layout>
-                    <v-flex class="pages-container">
-                        <div class="page-nav-buttons">
-                            <button class="page-nav-button previous text-xs-left" @click="prevPage()">
-                                <v-icon>mdi-arrow-left</v-icon>
-                            </button>
-                            <button class="page-nav-button next text-xs-right" @click="nextPage()">
-                                <v-icon>mdi-arrow-right</v-icon>
-                            </button>
-                        </div>
+            <v-flex xs12 md9 lg10>
+                <v-layout class="pages-container">
+                    <div class="page-nav-buttons">
+                        <button class="page-nav-button previous text-xs-left" @click="prevPage()">
+                            <i class="fa fa-fw fa-2x fa-chevron-left"></i>
+                        </button>
+                        <button class="page-nav-button next text-xs-right" @click="nextPage()">
+                            <i class="fa fa-fw fa-2x fa-chevron-right"></i>
+                        </button>
+                    </div>
+                    <v-flex lg6 class="text-xs-center text-lg-left">
                         <div class="page-container main">
                             <div class="page-heading">
                                 <h6 class="page-title text-xs-right">{{currentPages.main}}</h6>
@@ -31,6 +31,8 @@
                                 <img class="page-image elevation-8" :src="`/assets/images/comic/${currentPages.main}`" :alt="currentPages.main">
                             </div>
                         </div>
+                    </v-flex>
+                    <v-flex lg6 class="hidden-md-and-down">
                         <div class="page-container">
                             <div class="page-heading">
                                 <h6 class="page-title text-xs-right">{{currentPages.next}}</h6>
