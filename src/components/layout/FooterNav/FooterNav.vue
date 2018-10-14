d8c7a3faa602a8e05f2ecd32c0f03bb84b747ce9
cypherix93 <bikram.ghosh93@gmail.com>
2018-10-13 22:58:33 -0400

    Added footer nav.

---
+++ src/components/layout/FooterNav/FooterNav.vue
@@ -0,0 +1,20 @@
+<style lang="scss" src="./FooterNav.scss"></style>
+
+<template>
+    <div class="FooterNavComponent container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-auto">
+                <a href="https://github.com/albavulpes/web">
+                    <i class="fa fa-lg fa-github"></i>
+                    Github Project
+                </a>
+            </div>
+            <div class="col-auto">
+                &copy; {{new Date().getFullYear()}} albavulpes. All rights reserved.
+            </div>
+            <div class="col-auto">
+                Maintained by <a href="https://github.com/cypherix93">cypherix93</a>
+            </div>
+        </div>
+    </div>
+</template>
--- src/components/layout/TopNav/TopNav.vue
+++ src/components/layout/TopNav/TopNav.vue
@@ -1,39 +1,36 @@
-<script lang="ts" src="./TopNav.ts"></script>
 <style lang="scss" src="./TopNav.scss"></style>
 
 <template>
-    <div class="TopNavComponent">
-        <nav class="navbar navbar-expand fixed-top navbar-dark bg-primary shadow-3">
-            <router-link class="navbar-brand" :to="{name: 'home'}">
-                <div class="row align-items-center">
-                    <div class="col">
-                        <img class="brand-logo" src="../../../assets/images/logo_shadow.png" alt="AlbaVulpes Logo">
-                    </div>
-                    <div class="col">
-                        <div class="h3 brand-name">
-                            Alba Vulpes
-                        </div>
+    <nav class="TopNavComponent navbar navbar-expand fixed-top navbar-dark bg-primary shadow-3">
+        <router-link class="navbar-brand" :to="{name: 'home'}">
+            <div class="row align-items-center">
+                <div class="col">
+                    <img class="brand-logo" src="../../../assets/images/logo_shadow.png" alt="AlbaVulpes Logo">
+                </div>
+                <div class="col">
+                    <div class="h3 brand-name">
+                        Alba Vulpes
                     </div>
                 </div>
-            </router-link>
+            </div>
+        </router-link>
 
-            <ul class="navbar-nav d-none d-md-flex">
-                <li class="nav-item active">
-                    <router-link class="btn nav-link" :to="{name: 'home'}">
-                        Home
-                    </router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link class="btn nav-link" :to="{name: 'comics'}">
-                        Comics
-                    </router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link class="btn nav-link" :to="{name: 'reader'}">
-                        Reader
-                    </router-link>
-                </li>
-            </ul>
-        </nav>
-    </div>
+        <ul class="navbar-nav d-none d-md-flex">
+            <li class="nav-item active">
+                <router-link class="btn nav-link" :to="{name: 'home'}">
+                    Home
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="btn nav-link" :to="{name: 'comics'}">
+                    Comics
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="btn nav-link" :to="{name: 'reader'}">
+                    Reader
+                </router-link>
+            </li>
+        </ul>
+    </nav>
 </template>
