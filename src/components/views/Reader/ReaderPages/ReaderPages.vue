c9f4867a1915f0bbd87056abd34f2353a8e1784b
cypherix93 <bikram.ghosh93@gmail.com>
2019-04-04 22:24:26 -0400

    Transition for images

--- src/components/views/Reader/ReaderPages/ReaderPages.vue
+++ src/components/views/Reader/ReaderPages/ReaderPages.vue
@@ -5,21 +5,23 @@
     <div class="ReaderPagesComponent">
         <div class="pagesContainer d-flex align-items-center justify-content-center">
             <div class="pageContent">
-                <img class="pageImage card border-0 shadow-4" :src="Page.CoverImage.FullSize" alt="Current Page">
+                <transition name="fade" mode="out-in">
+                    <img class="pageImage card border-0 shadow-4" :src="Page.CoverImage.FullSize" alt="Current Page" :key="Page.Id">
+                </transition>
             </div>
         </div>
 
         <div class="pageNavButtons">
             <div class="d-flex h-100">
                 <div class="col-6 col-lg-3 p-0">
-                    <b-button block class="page-nav-button text-left text-white previous" @click="GoToPrevPage">
+                    <button class="page-nav-button btn-block text-left text-white previous" @click="GoToPrevPage">
                         <i class="mdi mdi-48px mdi-chevron-left"></i>
-                    </b-button>
+                    </button>
                 </div>
                 <div class="col-6 col-lg-3 p-0 ml-auto">
-                    <b-button block class="page-nav-button text-right text-white next" @click="GoToNextPage">
+                    <button class="page-nav-button btn-block text-right text-white next" @click="GoToNextPage">
                         <i class="mdi mdi-48px mdi-chevron-right"></i>
-                    </b-button>
+                    </button>
                 </div>
             </div>
         </div>
