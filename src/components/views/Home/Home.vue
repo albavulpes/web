00aff8707ad043e23667ad535284d6fd4225e694
cypherix93 <bikram.ghosh93@gmail.com>
2019-05-15 23:22:07 -0400

    new home page

--- src/components/views/Home/Home.vue
+++ src/components/views/Home/Home.vue
@@ -1,22 +1,53 @@
 <script lang="ts" src="./Home.ts"></script>
+<style lang="scss" src="./Home.scss"></style>
 
 <template>
-    <div class="container">
-        <div class="card bg-dark text-white">
-            <div class="card-body">
-                <h1>
-                    Welcome to the Home Page!
-                </h1>
-                <hr class="border-light">
-                <h3>
-                    Go to the following links for more awesome placeholders:
-                </h3>
-                <router-link class="btn btn-primary" :to="{name: 'reader'}">
-                    Reader
-                </router-link>
-                <router-link class="btn btn-secondary" :to="{name: 'comics'}">
-                    Comics
-                </router-link>
+    <div class="HomeComponent">
+        <div class="topBanner">
+            <div class="topBannerOverlay d-flex align-items-end justify-content-end">
+                <div class="topBannerTitles text-right">
+                    <div class="d-flex align-items-center">
+                        <div>
+                            <h1 class="topBannerMainTitle text-white">
+                                Alba Vulpes
+                            </h1>
+
+                            <p class="topBannerSubTitle text-white font-italic font-weight-lighter">
+                                Morality, like art, means drawing a line someplace
+                            </p>
+                        </div>
+                        <div class="px-3">
+                            <img src="https://albavulpes-common-assets.s3.amazonaws.com/brand/logo_shadow.png" alt="Alba Vulpes Logo" height="160">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container mainContent">
+            <div class="card bg-dark text-white">
+                <div class="card-body">
+                    <h1>
+                        Welcome!
+                    </h1>
+                    <hr class="border-light">
+
+                    <div class="d-flex justify-content-center">
+                        <b-card class="bg-transparent text-center">
+                            <i class="mdi mdi-library-books" style="font-size: 84px"></i>
+
+                            <br>
+
+                            <p class="my-3 text-white">
+                                I'm an aspiring artist that likes to draw and do art stuff. I dont know what you do, put a better "bio" line here.
+                            </p>
+
+                            <router-link class="btn btn-primary mt-3" :to="{name: 'comics'}">
+                                Check out my Comics
+                            </router-link>
+                        </b-card>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
